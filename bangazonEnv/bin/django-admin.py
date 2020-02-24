f6d42d73d0dfb557823eac17167fa44c665f149d
Schatfield <dgoldmine@icloud.com>
2020-02-24 15:10:19 -0600

    committing changes after virtual env changes

--- bangazonEnv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/shawnachatfield/workspace/pythonTime/AbracadabrasSprint2/bangazon_customer_api/bangazonEnv/bin/python
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
