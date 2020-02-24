9c024e0233580d250d4caca4531722604365c8d6
Schatfield <dgoldmine@icloud.com>
2020-02-24 14:47:18 -0600

    committing changes before checking a PR

---
+++ bangazonEnv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/shawnachatfield/workspace/pythonTime/AbracadabrasSprint2/bangazon_customer_api/bangazonEnv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
