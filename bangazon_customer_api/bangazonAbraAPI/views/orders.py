5c41c9dc1be2665c6ac51e0578ce8fa31e61020b
Matthew Blagg <matthewscottblagg@gmail.com>
2020-02-25 14:47:24 -0600

    order view

--- bangazon_customer_api/bangazonAbraAPI/views/orders.py
+++ bangazon_customer_api/bangazonAbraAPI/views/orders.py
@@ -1,18 +1,18 @@
 from django.http import HttpResponseServerError
+from bangazonAPI.models import Order, PaymentType, Customer
 from rest_framework.viewsets import ViewSet
+import datetime
+from rest_framework import status
 from rest_framework.response import Response
 from rest_framework import serializers
-from rest_framework import status
-from bangazonAPI.models import Order, PaymentType, Customer
 
 class OrderSerializer(serializers.HyperlinkedModelSerializer):
     """
     JSON serializer for Orders.py
     
-    Feeding in Args:
-        serializers
+    Feeding in Args for serializers
         
-    This is a Jeremiah Bell Disaster
+    This is a Jeremiah Bell/Matthew Scott Blagg Disaster
     """
     class Meta:
         model = Order
@@ -21,5 +21,60 @@ class OrderSerializer(serializers.HyperlinkedModelSerializer):
             lookup_field = 'id'
         )
         
-        fields = ('id',)
+        fields = ('url', 'id', 'products' 'created_at', 'payment_type')
+
+class Order(ViewSet):
+
+    def orderCreate (self, request):
+        """order instances returned and posts"""
     
+        neworder = Order()
+        neworder.customer_id = request.auth.user.customer.id
+        neworder.payment_type_id = request.data["payment_type_id"]
+        neworder.prodcuts= request.data["products"]
+        neworder.created_at = requested.data["created_at"]
+        neworder.save()
+        serializer = OrderSerializer(neworder, context = {"request": request})
+        return Response(serializer.data)
+
+    def orderRetrieve(self, request, pk = None):
+        """single order request"""
+
+        try: 
+            order = Order.objects.get(pk = pk)
+            serializer = OrderSerializer(order, context = {"request": request})
+            return Response(serializer.data)
+        except Exception as ex:
+            return HttpResponseServerError(ex)
+
+    def orderList(self, request):
+        """list of orders"""
+
+        orders = Order.objects.all()
+        serializer = OrderSerializer(orders, many = True, context = {"request": request})
+        return Response(serializer.data)
+
+    def orderUpdate(self, request, pk = None):
+        """Put for orders"""
+
+        order = Order()
+        order.customer_id = request.auth.user.customer.id
+        order.payment_type = request.data["payment_type"]
+        order.products = request.data["products"]
+        order.created_at = request.data["created_at"]
+        order.save()
+        return Response({}, status = status.HTTP_204_NO_CONTENT)
+
+    def orderDestroy(self, request, pk = None):
+        """order delete"""
+
+        try: 
+            Order = Order.objects.get(pk = pk)
+            Order.delete()
+            return Response({}, status = status.HTTP_204_NO_CONTENT)
+
+        except Order.DoesNotExist as ex:
+            return Response({"message": ex.args[0]}, status = status.HTTP_404_NOT_FOUND)
+
+        except Exception as ex:
+            return Response({"message": ex.args[0]}, status = status.HTTP_500_INTERNAL_SERVER_ERROR)
