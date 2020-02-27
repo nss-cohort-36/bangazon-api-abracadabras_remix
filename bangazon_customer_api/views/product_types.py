4b9ec2796286df1eb2b5de87164f07269c6fa5f9
Jisie DM <jisie.david@gmail.com>
2020-02-27 11:23:49 -0600

    New endpoint to retrieve a single product type

--- bangazon_customer_api/views/product_types.py
+++ bangazon_customer_api/views/product_types.py
@@ -2,6 +2,7 @@ from rest_framework.viewsets import ViewSet
 from bangazon_customer_api.models import ProductType
 from rest_framework import serializers
 from rest_framework.response import Response
+from django.http import HttpResponseServerError
 
 class ProductTypeSerializer(serializers.HyperlinkedModelSerializer):
     """JSON serializer for product types
@@ -34,3 +35,18 @@ class ProductTypes(ViewSet):
         )
         return Response(serializer.data)
         
+    def retrieve(self, request, pk=None):
+        """Handle GET requests to product types resource for a single product type
+
+        Returns:
+            Response -- JSON serialized list of park areas
+        """
+        try:
+            product_type = ProductType.objects.get(pk=pk)
+            serializer = ProductTypeSerializer(
+                product_type,
+                context={'request': request}
+            )
+            return Response(serializer.data)
+        except Exception as ex:
+            return HttpResponseServerError(ex)
