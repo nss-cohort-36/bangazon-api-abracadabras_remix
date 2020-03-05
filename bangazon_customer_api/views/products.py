7baccaa82125627890fe2e0cc48784260c93447d
Schatfield <dgoldmine@icloud.com>
2020-03-05 11:03:01 -0600

    completed products view, includes list, retrieve, update, create, and destroy

--- bangazon_customer_api/views/products.py
+++ bangazon_customer_api/views/products.py
@@ -23,13 +23,16 @@ class ProductsSerializer(serializers.HyperlinkedModelSerializer):
             lookup_field='id'
         )
         fields = ('id', 'url', 'name', 'price', 'description',
-                  'quantity', 'location', 'image_path', 'customer', 'product_type', )
+                  'quantity', 'location', 'image_path', 'customer', 'product_type', 'created_at')
         depth = 1
 
 
 class Products(ViewSet):
     """products for bangazon"""
 
+
+    # Get a Single Item 
+    # products/1
     def retrieve(self, request, pk=None):
         """Handle GET requests for single product
         Returns:
@@ -43,6 +46,8 @@ class Products(ViewSet):
         except Exception as ex:
             return HttpResponseServerError(ex)
 
+    # handles GET all
+    # /products
     def list(self, request):
         """Handle GET requests to products resource
         Returns:
@@ -63,6 +68,71 @@ class Products(ViewSet):
             products, many=True, context={'request': request})
         return Response(serializer.data)
 
+    # handles POST to make a new product
+    # products
+
+    def create(self, request):
+        """Handle POST operations
+
+        Returns:
+            Response -- JSON serialized ParkArea instance
+        """
+        newproduct = Product()
+        newproduct.name = request.data["name"]
+        newproduct.price = request.data["price"]
+        newproduct.description = request.data["description"]
+        newproduct.quantity = request.data["quantity"]
+        newproduct.location = request.data["location"]
+        newproduct.image_path = request.data["image_path"]
+        newproduct.created_at = request.data["created_at"]
+        newproduct.customer_id = request.data["customer_id"]
+        newproduct.product_type_id = request.data["product_type_id"]
+        newproduct.save()
+
+        serializer = ProductsSerializer(newproduct, context={'request': request})
+        # here you are instantiating the  serializer- you are making an instance of a product. you are also making the serializer available within in the scope of the class
+        return Response(serializer.data)
+        # Response contains data as a property on serializer, which contains the serialized queryset
+
+      # handles PUT to edit
+
+    def update(self, request, pk=None):
+        """Handle PUT requests for a product
+
+        Returns:
+            Response -- Empty body with 204 status code
+        """
+        product = Product.objects.get(pk=pk)
+        product.name = request.data["name"]
+        product.price = request.data["price"]
+        product.description = request.data["description"]
+        product.quantity = request.data["quantity"]
+        product.location = request.data["location"]
+        product.image_path = request.data["image_path"]
+        product.created_at = request.data["created_at"]
+        product.customer_id = request.data["customer_id"]
+        product.product_type_id = request.data["product_type_id"]
+        product.save()
+
+        serializer = ProductsSerializer(product, context={'request': request})
+        return Response(serializer.data)
 
 
+    def destroy(self, request, pk=None):
+        """Handle DELETE requests for a single product
+
+        Returns:
+            Response -- 200, 404, or 500 status code
+        """
+        try:
+            product = Product.objects.get(pk=pk)
+            product.delete()
+
+            return Response({}, status=status.HTTP_204_NO_CONTENT)
+
+        except Product.DoesNotExist as ex:
+            return Response({'message': ex.args[0]}, status=status.HTTP_404_NOT_FOUND)
+
+        except Exception as ex:
+            return Response({'message': ex.args[0]}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
         
