df9a423d152c8fc4089e0decadd5f7ee851e37d5
Jeremiah Bell <jeremiah.bell812.topper@gmail.com>
2020-03-08 21:07:28 -0600

    lists are broken for some reason need to diagnose the issue

--- bangazon_customer_api/views/customers.py
+++ bangazon_customer_api/views/customers.py
@@ -5,6 +5,7 @@ from rest_framework.response import Response
 from rest_framework import serializers
 from rest_framework import status
 from bangazon_customer_api.models import Customer
+from django.contrib.auth.models import User
 
 # Serializer
 class CustomerSerializer(serializers.HyperlinkedModelSerializer):
@@ -53,3 +54,19 @@ class Customers(ViewSet):
         user.save()
 
         return Response({}, status=status.HTTP_204_NO_CONTENT)
+    
+    # Creating a List Element so We can at least see a list of users if we want too, since this is the back end.
+    def list(self, request, pk=None):
+        """Handle GET request listing of customers endpoint
+        Returns:
+            Response: List of Customers as JSON
+        """
+        customers = Customer.objects.all()
+        customers = Customer.objects.filter(customer_id=request.auth.user.customer.id)
+
+        
+        # if customer is not None:
+        #     customers = customers.filter(id=customer)
+            
+        serializer = CustomerSerializer(customers, many=True, context={'request': request})
+        return Response(serializer.data)
